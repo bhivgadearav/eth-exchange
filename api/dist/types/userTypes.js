186e9fd3d3491fee235c44bd3d83ee12692ff1cc
Arav Bhivgade <hazeifygameplay5927@gmail.com>
2025-02-08 20:46:50 +0530

    Stopped commiting dist folder

--- api/dist/types/userTypes.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.TransactionStatus = void 0;
-var TransactionStatus;
-(function (TransactionStatus) {
-    TransactionStatus["PENDING"] = "PENDING";
-    TransactionStatus["SUCCESS"] = "SUCCESS";
-    TransactionStatus["FAILED"] = "FAILED";
-})(TransactionStatus || (exports.TransactionStatus = TransactionStatus = {}));
