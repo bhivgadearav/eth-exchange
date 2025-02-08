ad716783e566052b18718139a0d468b4c6a7b8d5
Arav Bhivgade <hazeifygameplay5927@gmail.com>
2025-02-08 21:52:39 +0530

    added instructions to make indexer 2

--- indexer/src/index.ts
+++ indexer/src/index.ts
@@ -1,9 +1,11 @@
-let CURRENT_BLOCK = 0;
+let BLOCK = 0;
 
 async function main() {
     // Get interested addresses from database
 
-    // Get the latest block number
+    // Get the 10th block behind latest block number
+    // Reason - ETH chain can be forked and to avoid
+    // processing data that's not in the main chain
 
     // Inspect the block for native eth txns 
     // involving the interested addresses
