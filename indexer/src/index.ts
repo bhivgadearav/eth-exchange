221d3a810fba7f142e409f15bc2a8c808f24531f
Arav Bhivgade <hazeifygameplay5927@gmail.com>
2025-02-08 21:36:36 +0530

    added instructions to make indexer

---
+++ indexer/src/index.ts
@@ -0,0 +1,10 @@
+let CURRENT_BLOCK = 0;
+
+async function main() {
+    // Get interested addresses from database
+
+    // Get the latest block number
+
+    // Inspect the block for native eth txns 
+    // involving the interested addresses
+}
