d4a5edbfe327889289ba2a923c023b80746572db
Arav Bhivgade <hazeifygameplay5927@gmail.com>
2025-02-09 22:10:20 +0530

    indexer done partially
    indexer can fetch details of a transaction in hardcoded block number of a hard coded address

--- indexer/src/index.ts
+++ indexer/src/index.ts
@@ -1,12 +1,80 @@
-let BLOCK = 0;
+import { PrismaClient } from '@prisma/client';
+import { JsonRpcProvider } from "ethers";
+
+const prisma = new PrismaClient();
+const provider = new JsonRpcProvider(process.env.ALCHEMY_ETH_RPC_URL);
+
+let BLOCK = 21809678;
+let ADDRESSES = ["0x2A088A920bd24f3512dA9A6398bf8da11F39fEDe"];
+
+async function getInterestedAddresses() {
+    // Fetch all users from database
+    const data = await prisma.user.findMany();
+
+    // Fetch all wallets from database
+    let walletIds = data.map((user) => user.mainWalletId);
+    let wallets = await prisma.wallet.findMany({
+        where: {
+            id: {
+                in: walletIds
+            }
+        }
+    });
+
+    // Extract public keys from wallets
+    ADDRESSES = wallets.map((wallet) => wallet.publicKey);
+    console.log(`Interested addresses: ${ADDRESSES}`);
+}
+
+async function getLatestBlock() {
+    const latestBlock = await provider.getBlockNumber();
+    BLOCK = latestBlock;
+    console.log(`Latest block: ${latestBlock}`);
+}
+
+
 
 async function main() {
     // Get interested addresses from database
+    // await getInterestedAddresses();
 
     // Get the 10th block behind latest block number
     // Reason - ETH chain can be forked and to avoid
     // processing data that's not in the main chain
+    // await getLatestBlock();
 
     // Inspect the block for native eth txns 
     // involving the interested addresses
+    console.log(`Inspecting block: ${BLOCK}`);
+    const block = await provider.getBlock(BLOCK, true);
+    const txns = block?.transactions;
+    
+    if (!txns) {
+        console.log("No transactions found");
+        return;
+    }
+
+    for (let txn of txns) {
+        const txnDetails = await provider.getTransaction(txn);
+        if (txnDetails?.to === ADDRESSES[0] || txnDetails?.from === ADDRESSES[0]) {
+            console.log(`Transaction Details:
+                Hash: ${txnDetails.hash}
+                From: ${txnDetails.from}
+                To: ${txnDetails.to}
+                Value: ${txnDetails.value.toString()}
+                Gas Price: ${txnDetails.gasPrice.toString()}
+                Gas Limit: ${txnDetails.gasLimit.toString()}
+                Nonce: ${txnDetails.nonce}
+                Data: ${txnDetails.data}
+                Block Number: ${txnDetails.blockNumber}
+                Confirmations: ${txnDetails.confirmations}
+            `);
+            return;
+        }
+    }
+
+    console.log("No transactions found for the adresses.");
 }
+
+// Run the script
+main();
